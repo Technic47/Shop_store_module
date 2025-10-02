9d105869bb6ae031b89e8a95e514caf286c4bf0e
Pavel Kuznetsov <Technic47@gmail.com>
2025-10-02 13:34:21 +0300

    Init commit

---
+++ src/main/java/ru/kuznetsov/shop_store_module/ShopStoreModuleApplication.java
@@ -0,0 +1,13 @@
+package ru.kuznetsov.shop_store_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopStoreModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopStoreModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kuznetsov/shop_store_module/ShopStoreModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kuznetsov.shop_store_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopStoreModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
